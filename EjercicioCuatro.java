e9cea8f4a4dc49da60a112185ee7b6cfcc7f475e
eduardoruedaroldan <31899937+eduardoruedaroldan@users.noreply.github.com>
2017-09-13 17:52:55 -0300

    proyectoTres

---
+++ EjercicioCuatro.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciocuatro;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioCuatro {
+
+    /**
+     * @param args the command line arguments
+     * se debe pedir el nombre y apellido y mostrar en el siguiente mensaje 
+     * su nombre es xxxx y su apellido xxxxy
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre;
+        String apellido;
+        
+        Scanner miScaner = new miScaner (system)
+    }
+    
+}
