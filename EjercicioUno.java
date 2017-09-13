2cb4516c6fbf4fce23cc410fc4626bf6e8edbefc
eduardoruedaroldan <31899937+eduardoruedaroldan@users.noreply.github.com>
2017-09-13 15:44:54 -0300

    tercerCommit
    mostre un mensaje utilizando source

--- EjercicioUno.java
+++ EjercicioUno.java
@@ -17,7 +17,8 @@ public class EjercicioUno {
      * esto funciona de maravilla
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     
+        System.out.println("esto funciona de maravilla");
     }
     
 }
