529bef13391d33a5745b66ca4520bdfad7eae030
eduardoruedaroldan <31899937+eduardoruedaroldan@users.noreply.github.com>
2017-09-13 17:52:28 -0300

    proyectoCuatro

---
+++ EjercicioCinco.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciocinco;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicioCinco {
+
+    /**
+     * @param args the command line arguments
+     * al ingresar una edad y el nombre se debe mostra el siguiente mensaje
+     * ustde se llama xxx y tiene xxx años de edad
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
