e95a4695bbf67d1443b872e12953249a553958c2
eduardoruedaroldan <31899937+eduardoruedaroldan@users.noreply.github.com>
2017-09-19 18:02:38 -0300

    ejercicioCinco

--- EjercicioCinco.java
+++ EjercicioCinco.java
@@ -5,6 +5,8 @@
  */
 package ejerciciocinco;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
@@ -13,11 +15,37 @@ public class EjercicioCinco {
 
     /**
      * @param args the command line arguments
-     * al ingresar una edad y el nombre se debe mostra el siguiente mensaje
+      al ingresar una edad y el nombre se debe mostra el siguiente mensaje
      * ustde se llama xxx y tiene xxx años de edad
      */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        //int NumeroUno;
+        //int NumeroDos;
+        //int Suma;
+        //Scanner 
+        //System.out.println("estoy en el ejercicio 5");
+        
+        //System.out.println("El resultado es: " + (Suma.in));
+        string nombre ;
+        int edad ;
+        
+        Scanner misca = new Scanner (System.in) ;
+        
+        System.out.println("ingrese el nombre: ");
+        
+        nombre = misca.nextLine();
+       
+        System.out.println("usted se llama : " + nombre);
+        
+        System.out.println("ingresar una edad: ");
+        
+        edad = misca.nextInt();
+        
+        System.out.println("usted tiene: " + edad);
+        
+               
+        
     }
     
 }
